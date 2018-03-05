409661d1678942a310019c74e99d3680614b329b
John Kasper Svergja <john.k.svergja@gmail.com>
2018-03-05 19:52:02 +0100

    added comments

--- frontend/src/AxiosInstance.js
+++ frontend/src/AxiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: 'http://localhost:5000/',
+    //For local development
+    // baseURL: 'http://localhost:8080/',
+    //For AWS:
     baseURL: '/',
 });
